46f96babc70440986bafa10503bd3216c180c3b8
adhowa <aubreyhowa@gmail.com>
2014-10-15 15:29:59 -0500

    finished 85 point

--- lab9-85pt.py
+++ lab9-85pt.py
@@ -8,9 +8,12 @@
 # and create a new list that includes only numbers greater than 100
 
 myList = [102,98,96,101,100,99,103,97,98,105]
-
+newList =[]
 # Insert for loop here
 
-
+for item in myList:
+    if item > 100:
+        newList.append(item) 
+print newList 
 # This should print [102,101,103,105]
-print myList
+ 
